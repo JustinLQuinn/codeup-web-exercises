d8f87a2768834f55635624ac09c8124d1a26a2d2
Justin Quinn <Justin.Quinn09@gmail.com>
2022-06-07 11:06:14 -0500

    moved file over from classroom repository folder into local students working project folder - kh

---
+++ js/for_loops.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// function showMultiplicationTable (num){
+// for (var i = 0; i < 10; i++){
+//    console.log(num+"*"+i+"="+(num*i))
+//
+// }
+// }
+
+// var ranNum = (Math.floor(Math.random() * 200) + 20);
+// for (let i = 0 ; i < 6; i++){
+//    console.log(ranNum % 2 === 0);{
+//          alert (ranNum+" is an Even number.");
+//       }else{
+//          alert (ranNum+" is a Odd number.");
+//       }
+//    }
+// for (let i = 1 ; i <= 10; i++){
+//    var ranNum = (Math.floor(Math.random() * 200) + 20);
+//    if (ranNum % 2 === 0){
+//       console.log(ranNum + " is an Even number.")
+//    }else{
+//       console.log(ranNum + " is an Odd number.")
+//    }
+//
+// }
+
+for (let i = 1; i <= 9; i++){
+   console.log(i)
+}
