ca5750d790493eff023fc6c346b52b90e9553980
Justin Quinn <Justin.Quinn09@gmail.com>
2022-06-07 15:59:01 -0500

    one last exercise to complete, while exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,35 +1,27 @@
-"use strict";
 
-//1st loop item#2
-// let user = Number(prompt ("Please enter a Odd number from 1-50:"));
 
-// let oddNumberCheck = (numberActual % 2 === 0);
-// let range = (numberActual>0 && numberActual<51);
-let user = Number(prompt("Please enter a Odd number from 1-50:"));
-
-while ( true) {
-//if num IS odd, then break
-    if (user % 2 !== 0) {
-        break;
-    }
-    alert("You have entered in a wrong variable value. Please try again.");
-    user = Number(prompt("Please enter a Odd number from 1-50:"));
-}
-//second loop
-for (let i = 0; i < 50; i++){
-    if (user % 2 === 0) {
-        break;
-    }
-    if (i === user){
-        console.log("skipping " + i);
-        continue
-    }
-    console.log(i);
+//1st loop
+// let user = Number(prompt("Please enter a Odd number from 1-50:"));
+//
+// while ( true) {
+// //if num IS odd, then break
+//     if (user % 2 !== 0) {
+//         break;
+//     }
+//     alert("You have entered in a wrong variable value. Please try again.");
+//     user = Number(prompt("Please enter a Odd number from 1-50:"));
+// }
+// //second loop
+// for (let i = 0; i < 50; i++){
+//     if (user % 2 === 0) {
+//         break;
+//     }
+//     if (i === user){
+//         console.log("skipping " + i);
+//         continue
+//     }
+//     console.log(i);
 }
 
- // for (let i = 1; i < 100; i++) {
- //     console.log();
- // }
 
-//2nd loop
 
---
+++ js/while.js
@@ -0,0 +1,10 @@
+"use strict";
+// let i = 1
+// while (i < 65536){
+//     i=(i * 2);
+//     console.log(i);
+// }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var soldCones = Math.floor(Math.random() * 5) + 1;
+
