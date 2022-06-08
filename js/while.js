f0042318cef669d722e6e10e1ab0267e6e951e50
Justin Quinn <Justin.Quinn09@gmail.com>
2022-06-08 10:28:50 -0500

    uncomment all code in while.js

--- js/while.js
+++ js/while.js
@@ -1,9 +1,9 @@
-"use strict";
-// let i = 1
-// while (i < 65536){
-//     i=(i * 2);
-//     console.log(i);
-// }
+
+let i = 1
+while (i < 65536){
+    i=(i * 2);
+    console.log(i);
+}
 
 function sellOut() {
     var allCones = Math.floor(Math.random() * 50) + 50;
