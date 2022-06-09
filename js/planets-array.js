76f716f15aa542b54ce7d18f46df230268425e87
Justin Quinn <Justin.Quinn09@gmail.com>
2022-06-09 11:03:32 -0500

    completted planets-array with satisfatory results

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,23 +18,29 @@
      * the step that it describes
      */
 
-    console.log(planets.unshift("The Sun");
+
+    console.log(planets.unshift("The Sun"));
     console.log(planets);
 
-    console.log('Adding "Pluto" to the end of the planets array.');
+    console.log(planets.push("Pluto"));
     console.log(planets);
 
-    console.log('Removing "The Sun" from the beginning of the planets array.');
+    console.log(planets.shift());
     console.log(planets);
 
-    console.log('Removing "Pluto" from the end of the planets array.');
+    console.log(planets.pop());
     console.log(planets);
 
-    console.log('Finding and logging the index of "Earth" in the planets array.');
+    var indexEarth = planets.indexOf('Earth');
+    console.log(indexEarth);
 
-    console.log("Reversing the order of the planets array.");
+    console.log(planets.reverse());
     console.log(planets);
 
-    console.log("Sorting the planets array.");
+    console.log(planets.sort());
     console.log(planets);
+
+    indexEarth = planets.indexOf('Earth')
+    console.log(indexEarth);
+
 })();
