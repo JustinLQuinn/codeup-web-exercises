677c7eeb85e57ec825afa39a4549c5c093efbddd
Justin Quinn <Justin.Quinn09@gmail.com>
2022-07-19 14:16:44 -0500

    added showing the Day Identifier for the weather cards

--- js/fiveDayForecast.js
+++ js/fiveDayForecast.js
@@ -16,75 +16,78 @@ function getForcast(lngLat) {
 }
 const weatherCardsInfo = document.getElementById("weatherCardsInfo")
 function weatherCards(dailyForecasts) {
+    let dts = [];
+    let dayName = [];
+    for (let i = 0; i < dailyForecasts.length; i++) {
+        dts.push(dailyForecasts[i].dt);
+        let date = (new Date(dailyForecasts[i].dt * 1000).toDateString()).split(" ");
+        dayName.push(date[0]);
+        console.log(dayName);
+    }
+
     weatherCardsInfo.innerHTML = `
     <div class="card">
         <div class="card-title">
-            <h2>${new Date(dailyForecasts[0].dt * 1000).toDateString()}</h2>
+            <h2>${dayName[0]}</h2>
         </div>
         <div class="card-body">
-                <li class="feature-item">Humidity: ${dailyForecasts[0].humidity}</li>
-                <li class="feature-item">Hi temp: ${parseInt(dailyForecasts[0].temp.max)}</li>
-                <li class="feature-item">Low temp: ${parseInt(dailyForecasts[0].temp.min)}</li>
-                <li class="feature-item">Feels like: ${parseInt(dailyForecasts[0].feels_like.day)}</li>
-                <li class="feature-item">Wind: ${parseInt(dailyForecasts[0].wind_gust)}</li>
+                Humidity: ${dailyForecasts[0].humidity}<br>
+                Hi temp: ${parseInt(dailyForecasts[0].temp.max)}<br>
+                Low temp: ${parseInt(dailyForecasts[0].temp.min)}<br>
+                Feels like: ${parseInt(dailyForecasts[0].feels_like.day)}<br>
+                Wind: ${parseInt(dailyForecasts[0].wind_gust)}</br>
         </div>
     </div>
     <div class="card">
         <div class="card-title">
-            <h2>${new Date(dailyForecasts[1].dt * 1000).toDateString()}</h2>
+            <h2>${dayName[1]}</h2>
         </div>
         <div class="card-body">
-            <ul class="feature-list">
-                <li class="feature-item">Humidity: ${dailyForecasts[1].humidity}</li>
-                <li class="feature-item">Hi temp: ${parseInt(dailyForecasts[1].temp.max)}</li>
-                <li class="feature-item">Low temp: ${parseInt(dailyForecasts[1].temp.min)}</li>
-                <li class="feature-item">Feels like: ${parseInt(dailyForecasts[1].feels_like.day)}</li>
-                <li class="feature-item">Wind: ${parseInt(dailyForecasts[1].wind_gust)}</li>
-            </ul>
+                Humidity: ${dailyForecasts[1].humidity}<br>
+                Hi temp: ${parseInt(dailyForecasts[1].temp.max)}<br>
+                Low temp: ${parseInt(dailyForecasts[1].temp.min)}<br>
+                Feels like: ${parseInt(dailyForecasts[1].feels_like.day)}<br>
+                Wind: ${parseInt(dailyForecasts[1].wind_gust)}<br>
         </div>
     </div>
     <div class="card">
         <div class="card-title">
-            <h2>${new Date(dailyForecasts[2].dt * 1000).toDateString()}</h2>
+            <h2>${dayName[2]}</h2>
         </div>
         <div class="card-body">
-            <ul class="feature-list">
-                <li class="feature-item">Humidity: ${dailyForecasts[2].humidity}</li>
-                <li class="feature-item">Hi temp: ${parseInt(dailyForecasts[2].temp.max)}</li>
-                <li class="feature-item">Low temp: ${parseInt(dailyForecasts[2].temp.min)}</li>
-                <li class="feature-item">Feels like: ${parseInt(dailyForecasts[2].feels_like.day)}</li>
-                <li class="feature-item">Wind: ${parseInt(dailyForecasts[2].wind_gust)}</li>
-            </ul>
+                Humidity: ${dailyForecasts[2].humidity}<br>
+                Hi temp: ${parseInt(dailyForecasts[2].temp.max)}<br>
+                Low temp: ${parseInt(dailyForecasts[2].temp.min)}<br>
+                Feels like: ${parseInt(dailyForecasts[2].feels_like.day)}<br>
+                Wind: ${parseInt(dailyForecasts[2].wind_gust)}<br>
         </div>
     </div>
     <div class="card">
         <div class="card-title">
-            <h2>${new Date(dailyForecasts[3].dt * 1000).toDateString()}</h2>
+            <h2>${dayName[3]}</h2>
         </div>
         <div class="card-body">
-            <ul class="feature-list">
-                <li class="feature-item">Humidity: ${dailyForecasts[3].humidity}</li>
-                <li class="feature-item">Hi temp: ${parseInt(dailyForecasts[3].temp.max)}</li>
-                <li class="feature-item">Low temp: ${parseInt(dailyForecasts[3].temp.min)}</li>
-                <li class="feature-item">Feels like: ${parseInt(dailyForecasts[3].feels_like.day)}</li>
-                <li class="feature-item">Wind: ${parseInt(dailyForecasts[3].wind_gust)}</li>
-            </ul>
+                Humidity: ${dailyForecasts[3].humidity}<br>
+                Hi temp: ${parseInt(dailyForecasts[3].temp.max)}<br>
+                Low temp: ${parseInt(dailyForecasts[3].temp.min)}<br>
+                Feels like: ${parseInt(dailyForecasts[3].feels_like.day)}<br>
+                Wind: ${parseInt(dailyForecasts[3].wind_gust)}<br>
         </div>
     </div>
     <div class="card">
         <div class="card-title">
-            <h2>${new Date(dailyForecasts[4].dt * 1000).toDateString()}</h2>
+            <h2>${dayName[4]}</h2>
         </div>
         <div class="card-body">
-            <ul class="feature-list">
-                <li class="feature-item">Humidity: ${dailyForecasts[4].humidity}</li>
-                <li class="feature-item">Hi temp: ${parseInt(dailyForecasts[4].temp.max)}</li>
-                <li class="feature-item">Low temp: ${parseInt(dailyForecasts[4].temp.min)}</li>
-                <li class="feature-item">Feels like: ${parseInt(dailyForecasts[4].feels_like.day)}</li>
-                <li class="feature-item">Wind: ${parseInt(dailyForecasts[4].wind_gust)}</li>
-            </ul>
+                Humidity: ${dailyForecasts[4].humidity}<br>
+                Hi temp: ${parseInt(dailyForecasts[4].temp.max)}<br>
+                Low temp: ${parseInt(dailyForecasts[4].temp.min)}<br>
+                Feels like: ${parseInt(dailyForecasts[4].feels_like.day)}<br>
+                Wind: ${parseInt(dailyForecasts[4].wind_gust)}<br>
         </div>
     </div>`
+
 }
+
 
 
--- js/homeForecast.js
+++ js/homeForecast.js
@@ -16,10 +16,19 @@
 
      const homeCardsInfo = document.querySelector(".footer");
      function homeWeather(dailyForecasts) {
+         let dts = [];
+         let dayName = [];
+         for (let i = 0; i < dailyForecasts.length; i++) {
+             dts.push(dailyForecasts[i].dt);
+             let date = (new Date(dailyForecasts[i].dt * 1000).toDateString()).split(" ");
+             dayName.push(date[0]);
+             console.log(dayName);
+         }
+
          homeCardsInfo.innerHTML = `
     <div class="card footer-card">
         <div class="card-title">
-            <h2>Today</h2>
+            <h2>${dayName[0]}</h2>
         </div>
         <div class="card-body">
             Humidity: ${dailyForecasts[0].humidity}<br>
@@ -32,7 +41,7 @@
     </div>
     <div class="card footer-card">
         <div class="card-title">
-            <h2>Day 2</h2>
+            <h2>${dayName[1]}</h2>
         </div>
         <div class="card-body">
                 Humidity: ${dailyForecasts[1].humidity}<br>
@@ -44,7 +53,7 @@
     </div>
     <div class="card footer-card">
         <div class="card-title">
-            <h2>Day 3</h2>
+            <h2>${dayName[2]}</h2>
         </div>
         <div class="card-body">
                 Humidity: ${dailyForecasts[2].humidity}<br>
@@ -56,7 +65,7 @@
     </div>
     <div class="card footer-card">
         <div class="card-title">
-            <h2>Day 4</h2>
+            <h2>${dayName[3]}</h2>
         </div>
         <div class="card-body">
                 Humidity: ${dailyForecasts[3].humidity}<br>
@@ -68,7 +77,7 @@
     </div>
     <div class="card footer-card">
         <div class="card-title">
-            <h2>Day 5</h2>
+            <h2>${dayName[4]}</h2>
         </div>
         <div class="card-body">
                 Humidity: ${dailyForecasts[4].humidity}<br>
@@ -78,5 +87,6 @@
                 Wind: ${parseInt(dailyForecasts[4].wind_gust)}<br>
         </div>
     </div>`
+
      }
  }
