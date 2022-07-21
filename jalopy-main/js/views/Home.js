bdb75e58d294d9a0930a1a8b65865edc138c88ba
Justin Quinn <Justin.Quinn09@gmail.com>
2022-07-21 17:03:07 -0500

    started by adding DogFacts page, now working on adding Quotes page

---
+++ jalopy-main/js/views/Home.js
@@ -0,0 +1,41 @@
+// import {showNotification} from "../messaging.js";
+// import {getUser} from "../auth.js";
+
+// const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
+
+const imagePaths = [
+    "assets/jalopy1.jpeg",
+    "assets/jalopy2.jpeg",
+    "assets/jalopy3.jpeg"
+]
+let currentImageIndex = 0;
+export default function homeHTMLFunction(props) {
+    return `
+        <header class="main-header">
+            <h1>Hello Jalopy!</h1>
+        </header>
+        <main>
+            <div class="container container1-main">
+                <img id="img1" class="homeimg" src="assets/jalopy1.jpeg">
+                <button class="changeimg">Change Me</button>
+            </div>
+            <div>
+                <p>
+                    Hello to my Jalopy application!
+                </p>    
+            </div>
+        </main>
+    `;
+}
+
+export function homeJSFunction() {
+    const button = document.querySelector(".changeimg");
+    button.addEventListener("click", function (event) {
+        const img = document.querySelector("#img1");
+        img.setAttribute("src", imagePaths[currentImageIndex]);
+        currentImageIndex++;
+        if (currentImageIndex === 3) {
+            currentImageIndex = 0;
+            }
+        })
+    }
